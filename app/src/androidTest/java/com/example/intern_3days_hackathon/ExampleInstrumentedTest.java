fd61944fcd4dc42d38318d8b2c077a2da58cd2f9
msugai-cq <m.sugai@caraquri.com>
2020-08-24 20:13:15 +0900

    delete ログイン画面

--- app/src/androidTest/java/com/example/intern_3days_hackathon/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/intern_3days_hackathon/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.intern_anrdoid_2020", appContext.getPackageName());
+        assertEquals("com.example.intern_3days_hackathon", appContext.getPackageName());
     }
 }
